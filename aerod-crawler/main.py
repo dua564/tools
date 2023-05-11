23a3ecf56e3d70ce55c81df96726d850fc0d0f74
Nikhil Dua <nikhil.dua@observeinc.com>
2023-05-10 18:00:09 -0700

    update

--- aerod-crawler/main.py
+++ aerod-crawler/main.py
@@ -6,6 +6,9 @@ import pandas as pd
 from collections import OrderedDict
 from datetime import datetime, timedelta
 import config
+import click
+import os
+from click.testing import CliRunner
 from helpers import *
 
 
@@ -60,10 +63,67 @@ def main():
     print(combined_df)
     send_email(combined_df)
 
+@click.command()
+@click.option('--planes', '-p', multiple=True, help="Comma separated list of planes (eg:-p 12234,61637")
+@click.option('--days', '-d', default=45, show_default=True, help="Future days to find")
+def cli_app(planes, days):
+
+    plane_tuple = planes
+    print(plane_tuple)
+    click.echo(','.join(planes))
+    click.secho("Days:{}".format(days), fg='blue')
+
+def app():
+    planes = os.environ.get('PLANES', '12234,54102').split(',')
+    days = int(os.environ.get('DAYS', '15'))
+    duration_hrs = int(os.environ.get('DURATION_HRS', '3'))
+
+    print("**********")
+    print ("SETTINGS:")
+    print ("   Planes to find:", planes)
+    print ("   Lookahead Days:", days)
+    print ("   Slot Duration Minimum:", duration_hrs)
+    print("**********")
+
+
+
+    #Initialize Empty DF
+    combined_df = pd.DataFrame()
+
+    # Create a session
+    session = requests.Session()
+
+    # Login First
+    response = session.post('https://aerod.paperlessfbo.com/FCMS1.aspx', params=config.params, cookies=config.cookies,
+                            headers=config.headers, data=config.data)
+
+    ####    Make a DF for all weekends in the next 45 days   ####
+    non_weekday_list = make_weekend_list(days_to_find=days)
+    for date in non_weekday_list:
+        print("Making DF for Date", date)
+        df = make_base_dataframe(date)
+        df = make_duration_df(df)
+        combined_df = pd.concat([combined_df, df], ignore_index=False)
+
+    ## Filter DF Based on Conditions
+    if 'ALL' in planes:
+        df_filtered = combined_df
+    else:
+        df_filtered = combined_df[combined_df['plane'].isin(planes)]
+
+    print(df_filtered)
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+  app()
+  #runner = CliRunner()
+  #result = runner.invoke(cli_app, ['--message', 'test'])
+   #pass
+
+   #cli_app()
    ##df = pd.read_csv('my_data.csv')
-   main()
+   ##main()
    #main_debug()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
